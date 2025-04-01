b06fa887aa8489a661fedbb799051ce3e8122ee9
Hisami Kurita <hisami.kurita@arsaga.jp>
2025-04-01 20:17:43 +0900

    🚨components

---
+++ src/components/card.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Card = () => {
+  return (
+    <div className="card">
+      <h2>Card Title</h2>
+      <p>This is a card component.</p>
+    </div>
+  );
+};
